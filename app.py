4a5394a87337f1a44c4f5a77a6a75d094d0970bd
danford christopher <jurvisdanford329@gmail.com>
2023-08-07 10:32:30 +0300

    trying to test if i will be able to run the flask application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1> Hello World </h1>"
+    return "<h1> Hello World Danford </h1>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
