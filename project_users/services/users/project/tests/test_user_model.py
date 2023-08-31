cfe07aed412d06fbd312b519a67f9d7742024278
danford christopher <jurvisdanford329@gmail.com>
2023-08-31 16:40:43 +0300

    setting up pyjwt for token authentication setting up authentication routes

--- project_users/services/users/project/tests/test_user_model.py
+++ project_users/services/users/project/tests/test_user_model.py
@@ -6,37 +6,56 @@ from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
+
 class TestUserModel(BaseTestCase):
     def test_add_user(self):
-        user = add_user('justatest', 'test@test.com')
+        user = add_user("justatest", "test@test.com", " test")
         self.assertTrue(user.id)
-        self.assertEqual(user.username, 'justatest')
-        self.assertEqual(user.email, 'test@test.com')
+        self.assertEqual(user.username, "justatest")
+        self.assertEqual(user.email, "test@test.com")
+        self.assertTrue(user.password)
         self.assertTrue(user.active)
 
     def test_add_user_duplicate_username(self):
-        add_user('justatest', 'test@test.com')
+        add_user("justatest", "test@test.com", "greaterthaneight")
         duplicate_user = User(
-        username='justatest',
-        email='test@test2.com',
+            username="justatest", email="test@test2.com", password="greaterthaneight"
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_add_user_duplicate_email(self):
-        add_user('justatest', 'test@test.com')
+        add_user("justatest", "test@test.com", "greaterthaneight")
         duplicate_user = User(
-        username='justatest2',
-        email='test@test.com',
+            username="justatest2",
+            email="test@test.com",
+            password="greaterthaneight",
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_to_json(self):
-        user = add_user('justatest', 'test@test.com')
+        user = add_user("justatest", "test@test.com", "greaterthaneight")
         self.assertTrue(isinstance(user.to_json(), dict))
+
+    def test_passwords_are_random(self):
+        user_one = add_user("justatest", "test@test.com", "greaterthaneight")
+        user_two = add_user("justatest2", "test@test2.com", "greaterthaneight")
+        self.assertNotEqual(user_one.password, user_two.password)
+
+    def test_encode_auth_token(self):
+        user = add_user("justatest", "test@test.com", "test")
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, str))
+
+    def test_decode_auth_token(self):
+        user = add_user("justatest", "test@test.com", "test")
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, str))
+        print(auth_token)
+        self.assertEqual(User.decode_auth_token(auth_token), user.id)
 
 
 # Remove the unnecessary __name__ check
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
